e2ef427be424cd29edbd6fa562954fffee04d2ed
tofiq7770 <tofigtn@code.edu.az>
2024-05-18 02:30:37 +0400

    add

---
+++ Fiorello/ViewModels/Home/HomeVM.cs
@@ -0,0 +1,19 @@
+﻿using Fiorello.Models;
+
+namespace Fiorello.ViewModels.Home
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+        public SliderInfo SliderInfos { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Surprize> Surprizes { get; set; }
+        public List<SurprizeList> SurprizeLists { get; set; }
+        public List<Expert> Experts { get; set; }
+        public List<Blog> Blogs { get; set; }
+        public List<Instagram> Instagrams { get; set; }
+
+
+    }
+}
